a9d53422ac2e6ea6374863ffa6ee968f0898ea37
student <mitch.murphy@gmail.com>
2020-11-21 17:33:08 +0000

    initial Spring app

---
+++ src/test/java/com/smigula/demoCI/DemoCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smigula.demoCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
